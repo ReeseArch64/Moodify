8692eebad11b3e592190bc9ae5b6522cb980f91e
Alan Guerra <195439767+ReeseArch64@users.noreply.github.com>
2025-04-15 14:59:55 -0300

    movendo arquivos

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        verde: "#32CD32", // Verde
+        claro: "#f4f4f9", // Branco Claro
+      },
+    },
+  },
+  plugins: [],
+};
